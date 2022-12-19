3e055e1bf951e663cee4a5e0a80cb3cc976a479a
Diego Ruiz Piqueras <diegopikeras@gmail.com>
2022-12-19 23:53:52 +0100

    Update Main.java

--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -1,28 +1,16 @@
 package model;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-
 public class Main {
 
-    public static void main (String [] args) throws Exception {
-
-        // Creación de la factoría
-
-        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure()
-                .build();
-
-        SessionFactory sessionFactory = new MetadataSources(registry)
-                .buildMetadata()
-                .buildSessionFactory();
-
-        Session session = sessionFactory.openSession();
-        //codigo insertar
-        session.close();
+    public static void main (String [] args) {
+        Controlador controlador = new Controlador();
+        provincia provincia = controlador.createProvincia("Madrid");
+        municipio municipio = controlador.createMunicipio("Madrid", provincia.getNombre_Provincia());
+        calle calle = controlador.createCalle("Calle de Guzmán el Bueno", municipio.getNombre_Municipio());
+        direccion direccion = controlador.createDireccion(32, 1, 'B', 0, calle.getNombre_Calle(), calle.getNombre_Municipio());
+        usuario_Identificado usuario_identificado = controlador.createUsuarioIdentificado("49841308F", "Pedro", "Alcocer Martín", "holasoypedro@gmail.com","12345678G");
+        System.out.println("Se ha creado un nuevo usuario_Identificado llamado: " + usuario_identificado.getNombre()+" "+usuario_identificado.getApellidos() + " cuyo DNI es " + usuario_identificado.getDNI_Usuario_Identificado() + " y vive en " + direccion.toString());
+        controlador.getSession().close();
     }
 }
 
