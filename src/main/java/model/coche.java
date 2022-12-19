031b0d33478add1320591775937eaa4a763faf54
Diego Ruiz Piqueras <diegopikeras@gmail.com>
2022-12-19 23:52:45 +0100

    Update coche.java

--- src/main/java/model/coche.java
+++ src/main/java/model/coche.java
@@ -1,9 +1,23 @@
 package model;
+
+import javax.persistence.*;
+import java.util.Set;
+
 public class coche {
 
+    @Id
+    @Column(name = "Matricula")
     private String Matricula;
-    private float Capacidad;
+
+    @OneToMany(mappedBy = "coche", cascade = CascadeType.ALL)
+    private Set<reparto> repartos;
+
+    @ManyToOne(optional = false, cascade = CascadeType.ALL)
+    @JoinColumn(name = "Cod_Oficina")
     private String Cod_Oficina;
+
+    @Column(name = "Capacidad", nullable = false)
+    private float Capacidad;
 
     public coche(String matricula, float capacidad, String cod_Oficina) {
         Matricula = matricula;
