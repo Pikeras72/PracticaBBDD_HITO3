0f521286fbc3dccb32f3bf7f0030e0fd46651f6b
Diego Ruiz Piqueras <diegopikeras@gmail.com>
2022-12-19 23:55:36 +0100

    Update ruta.java

--- src/main/java/model/ruta.java
+++ src/main/java/model/ruta.java
@@ -1,11 +1,23 @@
 package model;
 
-public class ruta {
+import javax.persistence.*;
+import java.util.Set;
 
+@Entity
+@Table(name = "ruta")
+public class ruta {
+    @Id
+    @Column(name = "ID_Ruta")
     private int ID_Ruta;
 
+    @OneToMany(mappedBy = "ruta", cascade = CascadeType.ALL)
+    private Set<reparto> repartos;
     public ruta(int ID_Ruta) {
         this.ID_Ruta = ID_Ruta;
+    }
+
+    public ruta() {
+        //requerido
     }
 
     //getters y setters
