cb2d4f659708030455aed3df06dab07e637a6b45
pikeras72 <diegopikeras@gmail.com>
2022-12-19 18:46:45 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+
+    public static void main (String [] args) throws Exception {
+
+        // Creación de la factoría
+
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+
+        SessionFactory sessionFactory = new MetadataSources(registry)
+                .buildMetadata()
+                .buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+        //codigo insertar
+        session.close();
+    }
+}
+
