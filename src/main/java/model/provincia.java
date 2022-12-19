c617a5a4d73788de726065f21fbf918983a4bd42
Diego Ruiz Piqueras <diegopikeras@gmail.com>
2022-12-19 23:54:51 +0100

    Update provincia.java

--- src/main/java/model/provincia.java
+++ src/main/java/model/provincia.java
@@ -1,12 +1,25 @@
 package model;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "provincia")
 public class provincia {
 
+    @Id
+    @Column(name = "Nombre_Provincia")
     private String Nombre_Provincia;
+
+    @OneToMany(mappedBy = "provincia", cascade = CascadeType.ALL)
+    private Set<municipio> municipios;
 
     public provincia(String nombre_Provincia) {
         Nombre_Provincia = nombre_Provincia;
+    }
 
+    public provincia() {
+        // requerido por Hibernate
     }
 
     //getters y setters
