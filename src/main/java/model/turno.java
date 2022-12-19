ebf71ac41e34d5dbb2628a686a78da609e3a69a3
Diego Ruiz Piqueras <diegopikeras@gmail.com>
2022-12-19 23:56:31 +0100

    Update turno.java

--- src/main/java/model/turno.java
+++ src/main/java/model/turno.java
@@ -1,17 +1,29 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.sql.Time;
-
+@Entity
+@Table(name = "turno")
 public class turno {
-
+    @Id
+    @Column(name = "Jornada")
     private String Jornada;
+    @Column(name = "Hora_inicio", nullable = false)
     private Time Hora_inicio;
+    @Column(name = "Hora_fin", nullable = false)
     private Time Hora_fin;
 
     public turno(String jornada, Time hora_inicio, Time hora_fin) {
         Jornada = jornada;
         Hora_inicio = hora_inicio;
         Hora_fin = hora_fin;
+    }
+
+    public turno() {
+        //requerido
     }
 
     //getters y setters
